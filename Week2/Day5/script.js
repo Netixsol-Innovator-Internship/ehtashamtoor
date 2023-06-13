20a6cc07d83eea60a4dc09bfb09b8985f69aa3f7
ehtashamNetixsol <135226252+ehtashamNetixsol@users.noreply.github.com>
2023-06-13 17:22:36 +0500

    Update script.js

--- Week2/Day5/script.js
+++ Week2/Day5/script.js
@@ -12,26 +12,52 @@ const body = document.querySelector("body");
 // click event to show COMPANY NAVLINK DROPDOWN
 CompanyBox.addEventListener("click", () => {
   insideListComp.classList.toggle("showList");
-  if (insideListComp.classList.contains("showList")) {
-    CompanyBtn.setAttribute("src", "images/icon-arrow-up.svg");
-  } else {
-    CompanyBtn.setAttribute("src", "images/icon-arrow-down.svg");
-  }
+  changeCompanyArrow();
 });
 // Click event to show Features Navlink dropdown
 FeaturesBox.addEventListener("click", () => {
   insideList.classList.toggle("showList");
+  changeFeatureArrow();
+});
+
+body.onclick = (e) => {
+  // console.log(e.target);
+  const tag = e.target;
+  //  remove showList class from company dropdown by clicking anywhere other than navlinks
+  if (
+    !CompanyBox.contains(tag) &&
+    insideListComp.classList.contains("showList")
+  ) {
+    insideListComp.classList.remove("showList");
+    changeCompanyArrow();
+  }
+
+  //  remove showList class from features dropdown by clicking anywhere other than navlinks
+  if (!FeaturesBox.contains(tag) && insideList.classList.contains("showList")) {
+    insideList.classList.remove("showList");
+    changeFeatureArrow();
+  }
+};
+
+const changeFeatureArrow = () => {
   if (insideList.classList.contains("showList")) {
     FeaturesBtn.setAttribute("src", "images/icon-arrow-up.svg");
   } else {
     FeaturesBtn.setAttribute("src", "images/icon-arrow-down.svg");
   }
-});
+};
+
+const changeCompanyArrow = () => {
+  if (insideListComp.classList.contains("showList")) {
+    CompanyBtn.setAttribute("src", "images/icon-arrow-up.svg");
+  } else {
+    CompanyBtn.setAttribute("src", "images/icon-arrow-down.svg");
+  }
+};
 
 // toggler button for navbar on small screens
 NavtoggleBtn.addEventListener("click", () => {
   navLinks.classList.toggle("showBtns");
-  body.classList.toggle("clickedMenu");
   if (navLinks.classList.contains("showBtns")) {
     DarkBg.style.display = "initial";
     NavtoggleBtn.setAttribute("src", "images/icon-close-menu.svg");
