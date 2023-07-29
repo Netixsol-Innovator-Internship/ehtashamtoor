e7de55f57e6697cc6c5f8b71ba4919aa814fdcb3
ehtashamtoor <ehtashamtoor_intern@netixsol.com>
2023-07-29 17:13:39 +0500

    Update index.js

--- Week6/day3/admin-dashboard/pages/index.js
+++ Week6/day3/admin-dashboard/pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   const { data: session } = useSession();
   return (
     <Layout>
+      
       <Toaster position="bottom-center" />
       <div className="text-blue-900 flex justify-between">
         <h2>
